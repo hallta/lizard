15658398c2baa49703ab703a0d80c51f190bacfe
Trevor Hall <hallta@gmail.com>
2017-05-18 17:15:56 +0100

    Working CSV output module, used with --csv switch

--- lizard_ext/csvoutput.py
+++ lizard_ext/csvoutput.py
@@ -1,10 +1,30 @@
 
+import pprint
+
 def print_csv(results, options, _):
-    print(csv_output(list(results), options.verbose))
+    csv_output(list(results), options.verbose)
     return 0
 
 def csv_output(result, verbose):
+    print "NLOC,CCN,token,PARAM,length,location,file,function,start,end"
     for source_file in result:
         if source_file:
-            print source_file.filename
-
+            for source_function in source_file.function_list:
+                if source_function:
+                    print('{},{},{},{},{},"{}","{}","{}","{}"'.format(
+                        source_function.nloc,
+                        source_function.cyclomatic_complexity,
+                        source_function.token_count,
+                        len(source_function.parameters),
+                        source_function.end_line - source_function.start_line,
+                        "{}@{}-{}@{}".format(
+                            source_function.name,
+                            source_function.start_line,
+                            source_function.end_line,
+                            source_file.filename
+                        ),
+                        source_file.filename,
+                        source_function.name,
+                        source_function.start_line,
+                        source_function.end_line
+                    ))
